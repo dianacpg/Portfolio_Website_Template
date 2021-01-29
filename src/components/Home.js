6dfa79d47e08254ec557e0bb0dd95d46241d1206
Diana Gouveia <dianacpgouveia@gmail.com>
2021-01-29 15:43:13 +0000

    bg again

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,6 @@ function Home () {
   return (
   <>
     <div className='background'>
-    <img src={process.env.PUBLIC_URL + '/images/reborned2.webp'} alt='colection' className='image__img' />
     </div>
     < Card />
     < Contact />
