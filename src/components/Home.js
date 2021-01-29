f0d2fcd54105c559a8ca50da9dac9179fdbdf0f1
Diana Gouveia <dianacpgouveia@gmail.com>
2021-01-29 15:28:31 +0000

    rm bg to img

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ function Home () {
   return (
   <>
     <div className='background'>
+    <img src={process.env.PUBLIC_URL + '/images/reborned2.webp'} alt='colection' className='image__img' />
     </div>
     < Card />
     < Contact />
